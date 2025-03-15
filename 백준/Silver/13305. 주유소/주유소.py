6104671f862497fe56813607e07f83f7bb618113
ungnam <ungnam0509@gmail.com>
2025-03-16 00:14:15 +0900

    [Silver III] Title: 주유소, Time: 92 ms, Memory: 42660 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+d = list(map(int, input().split()))
+p = list(map(int, input().split()))
+
+target = p[0]
+cost = p[0] * d[0]
+
+for i in range(n-2):
+  if p[i] > p[i+1]:
+    target = p[i+1]
+  cost += target * d[i+1]
+
+print(cost)
