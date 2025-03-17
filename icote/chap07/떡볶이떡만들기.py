9102b2146a1547dbde417f51376061b6e2781584
BEAR <ungnam0509@gmail.com>
2025-03-18 00:20:27 +0900

    fix: 코드 수정 및 주석 추가

--- icote/chap07/떡볶이떡만들기.py
+++ icote/chap07/떡볶이떡만들기.py
@@ -1,22 +1,28 @@
+# 입력 받기
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 
-# A.sort() 
-
-# 절단기의 높이를 이진 탐색으로 정해보자 (파라메트릭 서치)
+# 이진 탐색을 위한 시작점과 끝점 설정
+start, end = 0, max(A)
 result = 0
+
 while start <= end:
-  total = 0
-  mid = (start + end) // 2
-  for x in mid:
-    if x > mid:
-      total += x - mid
-  # 떡의 양이 부족하면 더 많이 자르기 (왼쪽 탐색)
-  if total < M:
-    end = mid - 1
-  # 떡의 양이 충분하다면 덜 자르기 (오른쪽 탐색)
-  else:
-    result = mid
-    start = mid + 1
+    total = 0
+    mid = (start + end) // 2
+
+    # 잘린 떡의 총 길이 계산
+    for x in A:
+        if x > mid:
+            total += x - mid
+
+    # 떡의 길이가 부족하면 더 많이 자르기 (왼쪽 탐색)
+    if total < M:
+        end = mid - 1
+    # 떡의 길이가 충분하면 덜 자르면서 최적 해 갱신 (오른쪽 탐색)
+    else:
+        # 조건을 만족했으니 일단 mid 값을 최적 해로 저장
+        result = mid
+        # 더 높은 값이 가능한지 확인하기 위해 오른쪽 탐색을 진행행
+        start = mid + 1
 
 print(result)
