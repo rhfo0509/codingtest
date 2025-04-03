0c319fca3a8f67535f6e35e1222d059828b4c4c1
ungnam <ungnam0509@gmail.com>
2025-04-03 17:21:30 +0900

    [Silver III] Title: 타노스, Time: 32 ms, Memory: 32412 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -0,0 +1,20 @@
+S = list(map(int, input()))
+
+c0 = S.count(0) // 2
+c1 = S.count(1) // 2
+
+i = len(S) - 1
+while c0:
+  if S[i] == 0:
+    del S[i]
+    c0 -= 1
+  i -= 1
+
+j = 0
+while c1:
+  if S[j] == 1:
+    del S[j]
+    c1 -= 1
+  j += 1
+
+print(''.join(map(str, S)))
