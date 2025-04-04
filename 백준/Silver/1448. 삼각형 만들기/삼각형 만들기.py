afb85bdbcc5e1d346f4327d55f471ca2016ff473
ungnam <ungnam0509@gmail.com>
2025-04-04 15:10:56 +0900

    [Silver III] Title: 삼각형 만들기, Time: 1260 ms, Memory: 77292 KB -BaekjoonHub

---
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+# 짧은 두 변의 길이의 합이 나머지 한 변의 길이보다 커야 한다.
+
+N = int(input())
+
+edges = []
+
+for i in range(N):
+  edges.append(int(input()))
+
+edges.sort()
+
+# 세 변의 길이의 합의 최댓값
+result = -1
+
+for i in range(N - 2):
+  if edges[i] + edges[i + 1] > edges[i + 2]:
+    result = sum(edges[i : i + 3])
+
+print(result)
