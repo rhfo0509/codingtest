2c8b4e69d5b9b7eff6fa6aa7cc5a1a69c5135331
ungnam <ungnam0509@gmail.com>
2025-04-10 17:49:01 +0900

    [Gold IV] Title: 세 수의 합, Time: 228 ms, Memory: 67232 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.py
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -6,26 +6,17 @@ for i in range(N):
   U.append(int(input()))
 U.sort()
 
-A=[]
+A=set()
 for x in range(N):
   for y in range(x,N):
-    A.append(U[x]+U[y])
-A.sort()
+    A.add(U[x]+U[y])
 
 i=-1
 for k in range(N-1,-1,-1):
   for z in range(k+1):
     target=U[k]-U[z]
-    start,end=0,len(A)-1
-    while start<=end:
-      mid=(start+end)//2
-      if A[mid]==target:
-        i=k
-        break
-      elif A[mid]<target:
-        start=mid+1
-      else:
-        end=mid-1
+    if target in A:
+      i=k
     if i!=-1:
       break
   if i!=-1:
