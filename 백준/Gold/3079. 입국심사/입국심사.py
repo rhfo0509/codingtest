b9ef80f7b88a6b83cbb964f4a93776a4bd19cd0c
ungnam <ungnam0509@gmail.com>
2025-04-14 16:11:07 +0900

    [Gold V] Title: 입국심사, Time: 1164 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Gold/3079. 입국심사/입국심사.py
@@ -0,0 +1,20 @@
+import sys
+input=sys.stdin.readline
+N,M=map(int,input().split())
+li=[]
+for _ in range(N):
+  li.append(int(input()))
+
+res=0
+start,end=0,max(li)*M
+while start<=end:
+  mid=(start+end)//2
+  cnt=0
+  for i in li:
+    cnt+=mid//i
+  if cnt>=M:
+    res=mid
+    end=mid-1
+  else:
+    start=mid+1
+print(res)
