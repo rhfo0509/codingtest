5022df03c818ba0413b6542fcf0b0a84ed59e40f
ungnam <ungnam0509@gmail.com>
2025-04-14 16:13:19 +0900

    [Gold V] Title: 입국심사, Time: 1116 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.py
+++ 백준/Gold/3079. 입국심사/입국심사.py
@@ -4,7 +4,7 @@ N,M=map(int,input().split())
 li=[]
 for _ in range(N):
   li.append(int(input()))
-
+li.sort()
 res=0
 start,end=0,max(li)*M
 while start<=end:
@@ -12,6 +12,8 @@ while start<=end:
   cnt=0
   for i in li:
     cnt+=mid//i
+    if cnt>=M:
+      break
   if cnt>=M:
     res=mid
     end=mid-1
