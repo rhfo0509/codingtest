55090f621b6df71448eaf00ff478f9a8cd6b1761
ungnam <ungnam0509@gmail.com>
2025-05-05 16:22:33 +0900

    [Gold IV] Title: N-Queen, Time: 19920 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -4,29 +4,23 @@ def n_queen(i):
   if i == N:
     cnt += 1
     return
-  # 이미 사용된 "칸"인지 체크
   for j in range(N):
-    isused = False
-    for pos in result:
-      qx, qy = pos
-      # 같은 "열"에 있는지 체크
-      if qy == j:
-        isused = True
-        break
-      # 같은 "대각선(╱)"에 있는지 체크
-      elif qx + qy == i + j:
-        isused = True
-        break
-      # 같은 "대각선(╲)"에 있는지 체크
-      elif qx - qy == i - j:
-        isused = True
-        break
-    if not isused:
-      result.append((i, j))
+    if not col[j] and not diag1[i + j] and not diag2[i - j + N - 1]:
+      col[j] = True
+      diag1[i + j] = True
+      diag2[i - j + N - 1] = True
       n_queen(i + 1)
-      result.pop()
+      col[j] = False
+      diag1[i + j] = False
+      diag2[i - j + N - 1] = False
     
 N = int(input())
 result = []
+# 해당 열에 퀸을 둘 수 있는지 여부
+col = [False] * N
+# 해당 대각선(╱)에 퀸을 둘 수 있는지 여부
+diag1 = [False] * (2 * N - 1)
+# 해당 대각선(╲)에 퀸을 둘 수 있는지 여부
+diag2 = [False] * (2 * N - 1)
 n_queen(0)
 print(cnt)
