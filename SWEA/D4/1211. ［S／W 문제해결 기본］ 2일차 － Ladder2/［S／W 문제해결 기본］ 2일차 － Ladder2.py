0ae70f98241905f12f1bd59ea67dd03d6fc2e5dd
ungnam <ungnam0509@gmail.com>
2025-05-08 18:03:22 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder2, Time: 128 ms, Memory: 61,056 KB -BaekjoonHub

---
+++ SWEA/D4/1211. ［S／W 문제해결 기본］ 2일차 － Ladder2/［S／W 문제해결 기본］ 2일차 － Ladder2.py
@@ -0,0 +1,38 @@
+for _ in range(10):
+  N=int(input())
+  data=[list(map(int,input().split())) for _ in range(100)]
+  result_idx, result_dist = 0, int(1e9)
+  for i in range(100):
+    if data[0][i] == 1:
+      # 출발
+      x, y = 0, i
+
+      # L, D, R 순서
+      dx = [0, 1, 0]
+      dy = [-1, 0, 1]
+
+      # 처음: 아래(D)
+      d = 1
+
+      # 움직인 거리
+      dist = 0
+
+      while True:
+        if x == 99:
+          break
+        if d == 1 and y > 0 and data[x][y - 1] == 1:
+          d = 0
+        elif d == 1 and y < 99 and data[x][y + 1] == 1:
+          d = 2
+        elif (d == 0 or d == 2) and data[x + 1][y] == 1:
+          d = 1
+
+        x += dx[d]
+        y += dy[d]
+        dist += 1
+
+      if dist < result_dist:
+        result_idx = i
+        result_dist = dist
+
+  print(f'#{N} {result_idx}')
