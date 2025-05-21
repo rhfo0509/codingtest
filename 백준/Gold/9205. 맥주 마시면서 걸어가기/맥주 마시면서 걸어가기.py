69f22aa4c823d7cc3b02b708236cb327d3d8ac42
ungnam <ungnam0509@gmail.com>
2025-05-21 16:48:22 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 112 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -4,20 +4,20 @@ def bfs(x,y):
   q=deque([(x,y)])
   while q:
     px,py=q.popleft()
-    if (px,py)==convs[-1]:
-      return True
+    if (px,py)==dest[-1]:
+      return 'happy'
     for i in range(n+1):
-      cx,cy=convs[i]
+      cx,cy=dest[i]
       if abs(px-cx)+abs(py-cy)<=1000 and not visited[i]:
         visited[i]=True
         q.append((cx,cy))
-  return False
+  return 'sad'
 
 for _ in range(t):
   n=int(input())
   home=tuple(map(int,input().split()))
-  convs=list(tuple(map(int,input().split())) for _ in range(n))
-  convs.append(tuple(map(int,input().split())))
+  dest=list(tuple(map(int,input().split())) for _ in range(n))
+  dest.append(tuple(map(int,input().split())))
   visited=[False]*(n+1)
   hx,hy=home
-  print('happy' if bfs(hx, hy) else 'sad')
+  print(bfs(hx, hy))
