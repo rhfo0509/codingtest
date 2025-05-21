b7c9be3e90599d1f2da378b9599b06fd54a7f88f
ungnam <ungnam0509@gmail.com>
2025-05-21 19:20:07 +0900

    [D3] Title: 햄버거 다이어트, Time: 133 ms, Memory: 74,240 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,19 +1,17 @@
 T=int(input())
-def dfs(start,score,calorie):
-  if calorie>L:
-    return
-  global result
-  result=max(result,score)
-  for i in range(start,N):
-    if not visited[i]:
-      S,C=data[i]
-      visited[i]=True
-      dfs(i+1,score+S,calorie+C)
-      visited[i]=False
 for tc in range(1,T+1):
   N,L=map(int,input().split())
-  data=[tuple(map(int,input().split())) for _ in range(N)]
-  visited=[False]*N
-  result=0
-  dfs(0,0,0)
-  print(f'#{tc} {result}')
+  # data=[tuple(map(int,input().split())) for _ in range(N)]
+  dp=[[0]*(L+1) for _ in range(N+1)]
+
+  for i in range(1,N+1):
+    score,calorie=map(int,input().split())
+    for j in range(1,L+1):
+      # 재료를 선택하지 않은 경우
+      dp[i][j]=dp[i-1][j]
+      # 재료를 선택한 경우
+      if j-calorie>=0:
+        dp[i][j]=max(dp[i][j],dp[i-1][j-calorie]+score)
+
+  # 모든 재료를 고를 수 있고, 칼로리 제한이 L일 때 가치합의 최댓값
+  print(f'#{tc} {dp[N][L]}')
