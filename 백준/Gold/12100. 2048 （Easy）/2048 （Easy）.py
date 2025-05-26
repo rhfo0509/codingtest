1bea9034132e8c8001941ea1343f79591dc62346
ungnam <ungnam0509@gmail.com>
2025-05-26 23:01:11 +0900

    [Gold I] Title: 2048 (Easy), Time: 484 ms, Memory: 33740 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -3,19 +3,17 @@ N = int(input())
 
 board = [list(map(int, input().split())) for _ in range(N)]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
 # (보드 정보, 방향)
 def move(data, d):
   # 합쳐진 블록인지 여부
   combined = [[False] * N for _ in range(N)]
 
+  # U, D -> transpose
   if d == 0 or d == 1:
     data = list(map(list, zip(*data)))
 
   for i in range(N):
-    # U(0), L(2) -> 정방향 순회
+    # U, L -> 정방향 순회
     if d == 0 or d == 2:
       for j in range(1, N):
         # 블록이 아니라면 continue
@@ -24,35 +22,24 @@ def move(data, d):
 
         cur = data[i][j]
         k = j
+        data[i][j] = 0
         while True:
-          # 블록을 발견하지 못했다면
-          if k == 0:
-            data[i][j] = 0
-            data[i][k] = cur
-            break
-
-          # 만약 현재 위치의 직전 블록이 결합된 블록이라면
-          if combined[i][k - 1]:
-            data[i][j] = 0
+          # 1. 블록을 발견하지 못했다면
+          # 2. 만약 현재 위치의 직전 블록이 결합된 블록이라면
+          # 3. 만약 현재 위치의 직전 블록과 같은 숫자가 아닌 블록을 발견했다면
+          if k == 0 or combined[i][k - 1] or (data[i][k - 1] and data[i][k - 1] != cur):
             data[i][k] = cur
             break
 
           # 만약 현재 위치의 직전 블록과 같은 숫자인 블록을 발견했다면
           if data[i][k - 1] == cur:
-            data[i][j] = 0
             data[i][k - 1] *= 2
             combined[i][k - 1] = True
             break
 
-          # 만약 현재 위치의 직전 블록과 같은 숫자가 아닌 블록을 발견했다면
-          if data[i][k - 1] and data[i][k - 1] != cur:
-            data[i][j] = 0
-            data[i][k] = cur
-            break
-
           k -= 1
 
-    # D(1), R(3) -> 역방향 순회
+    # D, R -> 역방향 순회
     else:
       for j in range(N - 2, -1, -1):
         # 블록이 아니라면 continue
@@ -61,37 +48,26 @@ def move(data, d):
 
         cur = data[i][j]
         k = j
+        data[i][j] = 0
         while True:
-          # 블록을 발견하지 못했다면
-          if k == N - 1:
-            data[i][j] = 0
-            data[i][k] = cur
-            break
-
-          # 만약 현재 위치의 직후 블록이 결합된 블록이라면
-          if combined[i][k + 1]:
-            data[i][j] = 0
+          # 1. 블록을 발견하지 못했다면
+          # 2. 만약 현재 위치의 직후 블록이 결합된 블록이라면
+          # 3. 만약 현재 위치의 직후 블록과 같은 숫자가 아닌 블록을 발견했다면 
+          if k == N - 1 or combined[i][k + 1] or (data[i][k + 1] and data[i][k + 1] != cur):
             data[i][k] = cur
             break
 
           # 만약 현재 위치의 직후 블록과 같은 숫자인 블록을 발견했다면
           if data[i][k + 1] == cur:
-            data[i][j] = 0
             data[i][k + 1] *= 2
             combined[i][k + 1] = True
             break
 
-          # 만약 현재 위치의 직후 블록과 같은 숫자가 아닌 블록을 발견했다면
-          if data[i][k + 1] and data[i][k + 1] != cur:
-            data[i][j] = 0
-            data[i][k] = cur
-            break
-
           k += 1
 
-  # 변경된 보드 정보 return
   if d == 0 or d == 1:
     data = list(map(list, zip(*data)))
+  # 변경된 보드 정보 return
   return data
 
 # (지금까지 이동한 횟수, 보드 정보, 방향)
