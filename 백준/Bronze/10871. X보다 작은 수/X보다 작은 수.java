8c792d222afe2a9ae3e1189cb8c9371f32e5276c
KIM EUNGSEO <ungnam0509@gmail.com>
2025-06-25 23:46:46 +0900

    [Bronze V] Title: X보다 작은 수, Time: 532 ms, Memory: 36292 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            int num = sc.nextInt();
+            if (num < X) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
