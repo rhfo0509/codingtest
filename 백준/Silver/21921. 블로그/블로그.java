e29029b6cdc58d7d66329b802bff04d967b3c7f8
KIM EUNGSEO <ungnam0509@gmail.com>
2025-07-03 18:58:16 +0900

    [Silver III] Title: 블로그, Time: 316 ms, Memory: 35148 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -1,15 +1,19 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int X = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st1.nextToken());
+        int X = Integer.parseInt(st1.nextToken());
         int[] arr = new int[N];
 
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st2.nextToken());
         }
 
         // 현재 방문자 수
