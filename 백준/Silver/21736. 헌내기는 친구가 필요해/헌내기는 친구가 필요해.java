1295cabbdeb160591ada7f21ad6eeda9a1a5fd40
KIM EUNGSEO <ungnam0509@gmail.com>
2025-07-06 00:59:39 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 616 ms, Memory: 57704 KB -BaekjoonHub

---
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	// 좌표 클래스
+	static class Point {
+		int x;
+		int y;
+		
+		Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	
+	public static int bfs(int x, int y, int r, int c, String[][] data) {
+		int count = 0;
+		boolean[][] visited = new boolean[r][c];
+		Deque<Point> q = new ArrayDeque<Point>();
+		
+		q.add(new Point(x, y));
+		visited[x][y] = true;
+		
+		while (!q.isEmpty()) {
+			Point node = q.poll();
+			int px = node.x;
+			int py = node.y;
+			if (data[px][py].equals("P")) {
+				count++;
+			}
+			
+			for (int i = 0; i < 4; i++) {
+				int nx = px + dx[i];
+				int ny = py + dy[i];
+				if (nx < 0 || nx >= r || ny < 0 || ny >= c || data[nx][ny].equals("X") || visited[nx][ny]) {
+					continue;
+				}
+				q.add(new Point(nx, ny));
+				visited[nx][ny] = true;
+			}
+		}
+		
+		return count;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		String[][] data = new String[n][m];
+		for (int i = 0; i < n; i++) {
+			String[] rowData = br.readLine().split("");
+			for (int j = 0; j < m; j++) {
+				data[i][j] = rowData[j];
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (data[i][j].equals("I")) {
+					int result = bfs(i, j, n, m, data);
+					if (result == 0) {
+						System.out.print("TT");
+					} else {
+						System.out.print(result);
+					}
+				}
+			}
+		}
+	}
+}
