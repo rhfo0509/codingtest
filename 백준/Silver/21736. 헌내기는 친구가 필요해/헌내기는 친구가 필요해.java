2a8cb9107dc4f90dce3fcf310a7762411b54b296
KIM EUNGSEO <ungnam0509@gmail.com>
2025-07-06 01:04:51 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 336 ms, Memory: 28424 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -16,7 +16,7 @@ public class Main {
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
 	
-	public static int bfs(int x, int y, int r, int c, String[][] data) {
+	public static int bfs(int x, int y, int r, int c, char[][] data) {
 		int count = 0;
 		boolean[][] visited = new boolean[r][c];
 		Deque<Point> q = new ArrayDeque<Point>();
@@ -28,14 +28,14 @@ public class Main {
 			Point node = q.poll();
 			int px = node.x;
 			int py = node.y;
-			if (data[px][py].equals("P")) {
+			if (data[px][py] == 'P') {
 				count++;
 			}
 			
 			for (int i = 0; i < 4; i++) {
 				int nx = px + dx[i];
 				int ny = py + dy[i];
-				if (nx < 0 || nx >= r || ny < 0 || ny >= c || data[nx][ny].equals("X") || visited[nx][ny]) {
+				if (nx < 0 || nx >= r || ny < 0 || ny >= c || data[nx][ny] == 'X' || visited[nx][ny]) {
 					continue;
 				}
 				q.add(new Point(nx, ny));
@@ -51,16 +51,16 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		String[][] data = new String[n][m];
+		char[][] data = new char[n][m];
 		for (int i = 0; i < n; i++) {
-			String[] rowData = br.readLine().split("");
+			String rowData = br.readLine();
 			for (int j = 0; j < m; j++) {
-				data[i][j] = rowData[j];
+				data[i][j] = rowData.charAt(j);
 			}
 		}
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				if (data[i][j].equals("I")) {
+				if (data[i][j] == 'I') {
 					int result = bfs(i, j, n, m, data);
 					if (result == 0) {
 						System.out.print("TT");
