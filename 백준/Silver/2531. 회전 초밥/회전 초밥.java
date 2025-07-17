f5e55ef6a2f5ff93b4b8a665e78d7d933f20b0f3
KIM EUNGSEO <ungnam0509@gmail.com>
2025-07-18 00:46:26 +0900

    [Silver I] Title: 회전 초밥, Time: 240 ms, Memory: 22360 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -17,23 +17,46 @@ public class Main {
 			data[i] = Integer.parseInt(br.readLine());
 		}
 		
-		HashSet<Integer> set = new HashSet<Integer>();
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		for (int i = 0; i < k; i++) {
+//			if (map.containsKey(data[i])) {
+//				map.put(data[i], map.get(data[i]) + 1);
+//			} else {
+//				map.put(data[i], 1);
+//			}
+			
+			map.put(data[i], map.getOrDefault(data[i], 0) + 1);
+			
+//			map.merge(data[i], 1, Integer::sum);
+		}
 		
 		// 초밥 가짓수의 최댓값
-		int result = 0;
+		int result = map.size();
 		
+		if (!map.containsKey(c)) {
+			result += 1;
+		}
+		
+		int from = 0;
+		int to = k - 1;
 		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < k; j++) {
-				set.add(data[(i + j) % N]);
+			
+			map.put(data[from], map.get(data[from]) - 1);
+			if (map.get(data[from]) == 0) {
+				map.remove(data[from]);
+			}
+			from += 1;
+			to = (to + 1) % N;
+			map.put(data[to], map.getOrDefault(data[to], 0) + 1);
+			
+			int count = map.size();
+			
+			if (!map.containsKey(c)) {
+				count += 1;
 			}
 			
-			// 쿠폰 번호가 set에 포함이 안되어 있으면 추가
-			if (!set.contains(c)) {
-				set.add(c);
-			}
-			
-			result = Math.max(result, set.size());
-			set.clear();
+			result = Math.max(result, count);
 		}
 		
 		System.out.print(result);
