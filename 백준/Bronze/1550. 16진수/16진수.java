89b7b105739ab33e4a80bb95f1ce5259954a81fc
KIM EUNGSEO <ungnam0509@gmail.com>
2025-07-17 23:57:08 +0900

    [Bronze II] Title: 16진수, Time: 112 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/1550. 16진수/16진수.java
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -5,13 +5,9 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		String numStr = br.readLine();
-		int num = 0;
-		for (int i = 0; i < numStr.length(); i++) {
-			num += Integer.parseInt(numStr.charAt(i) + "", 16) * Math.pow(16, numStr.length() - i - 1);
-		}
+		int num = Integer.parseInt(numStr, 16);
 		System.out.println(num);
 	}
 }
