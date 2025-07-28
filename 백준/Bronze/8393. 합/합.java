7221523be123258c47c232b1cb573f8721bde95d
KIM EUNGSEO <ungnam0509@gmail.com>
2025-07-28 17:59:04 +0900

    [Bronze V] Title: 합, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int sum = 0;
+		for (int i = 1; i <= N; i++) {
+			sum += i;
+		}
+		System.out.print(sum);
+	}
+
+}
