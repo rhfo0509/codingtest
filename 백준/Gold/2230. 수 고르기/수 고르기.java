1c1c4b6d0e79d1c276f98382e3168e299291449b
KIM EUNGSEO <ungnam0509@gmail.com>
2025-08-05 21:04:46 +0900

    [Gold V] Title: 수 고르기, Time: 304 ms, Memory: 27556 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -6,44 +6,33 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, M;
-	static int result = Integer.MAX_VALUE;
-	static int[] arr;
-	
-	public static void binarySearch(int idx, int target) {
-		// 이분탐색
-		int start = idx;
-		int end = N - 1;
-		int mid;
-		
-		while (start <= end) {
-			mid = (start + end) / 2;
-			int diff = arr[mid] - target;
-			if (diff >= M) {
-				result = Math.min(result, diff);
-				// 더 차이가 작은 값이 있는지 탐색
-				end = mid - 1;
-			} else {
-				start = mid + 1;
-			}
-		}
-	}
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
 		
-		arr = new int[N];
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 		Arrays.sort(arr);
 		
-		for (int i = 0; i < N; i++) {
-			int a = arr[i];
-			binarySearch(i + 1, a);
+		int start = 0;
+		int end = 0;
+		
+		int result = Integer.MAX_VALUE;
+		
+		while (end < N && start <= end) {
+			int diff = arr[end] - arr[start];
+			
+			if (diff >= M) {
+				result = Math.min(result, diff);
+				start += 1;
+			} else {
+				end += 1;
+			}
 		}
 		
 		System.out.print(result);
