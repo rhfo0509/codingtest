4ecc00aa809cbb11aac6d73734f13526b6169bcf
KIM EUNGSEO <ungnam0509@gmail.com>
2025-09-04 22:13:27 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 292 ms, Memory: 17984 KB -BaekjoonHub

---
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    // {내구도, 무게}
+    static int[][] eggs;
+    static boolean[] used;
+    static int result;
+
+    static void dfs(int idx) {
+        if (idx == N) {
+            int eggCount = 0;
+            for (int i = 0; i < N; i++) {
+                if (eggs[i][0] <= 0) eggCount++;
+            }
+//            System.out.println(eggCount);
+            result = Math.max(result, eggCount);
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (i == idx) continue;
+
+            if (eggs[i][0] <= 0 || eggs[idx][0] <= 0) {
+                dfs(idx + 1);
+                continue;
+            }
+            eggs[i][0] -= eggs[idx][1];
+            eggs[idx][0] -= eggs[i][1];
+
+            dfs(idx + 1);
+
+            eggs[i][0] += eggs[idx][1];
+            eggs[idx][0] += eggs[i][1];
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        eggs = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            eggs[i][0] = S;
+            eggs[i][1] = W;
+        }
+
+        result = 0;
+        used = new boolean[N];
+
+        if (N != 1) {
+            dfs(0);
+
+        }
+        System.out.println(result);
+    }
+}
