93c99a8c09db7cad1da75990aeb0baf1e3891f82
KIM EUNGSEO <ungnam0509@gmail.com>
2025-09-04 10:17:00 +0900

    [D3] Title: 햄버거 다이어트, Time: 133 ms, Memory: 74,240 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,17 +1,43 @@
-T=int(input())
-for tc in range(1,T+1):
-  N,L=map(int,input().split())
-  # data=[tuple(map(int,input().split())) for _ in range(N)]
-  dp=[[0]*(L+1) for _ in range(N+1)]
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-  for i in range(1,N+1):
-    score,calorie=map(int,input().split())
-    for j in range(1,L+1):
-      # 재료를 선택하지 않은 경우
-      dp[i][j]=dp[i-1][j]
-      # 재료를 선택한 경우
-      if j-calorie>=0:
-        dp[i][j]=max(dp[i][j],dp[i-1][j-calorie]+score)
+public class Solution {
+	
+	static int N, L;
+	// {맛에 대한 점수, 칼로리}
+	static int[][] dp;
 
-  # 모든 재료를 고를 수 있고, 칼로리 제한이 L일 때 가치합의 최댓값
-  print(f'#{tc} {dp[N][L]}')
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
+			
+			// dp[n][l]: 재료의 수가 n이고, 제한 칼로리가 l일때 가치합의 최댓값
+			dp = new int[N + 1][L + 1];
+			
+			for (int i = 1; i <= N; i++) {
+				st = new StringTokenizer(br.readLine());
+				int t = Integer.parseInt(st.nextToken());
+				int k = Integer.parseInt(st.nextToken());
+				for (int j = 1; j <= L; j++) {
+					// 1. 해당 재료를 선택하지 않는 경우
+					dp[i][j] = dp[i - 1][j];
+					if (j - k >= 0) {
+						// 2. 해당 재료를 선택하는 경우
+						dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - k] + t);
+					}
+				}
+			}
+			
+			System.out.printf("#%d %d\n", tc, dp[N][L]);
+		}
+	}
+
+}
