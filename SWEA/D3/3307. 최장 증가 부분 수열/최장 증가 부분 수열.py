4d2082a5f4a1d1ccd8aa091e7cb3b8a6261c0e96
KIM EUNGSEO <ungnam0509@gmail.com>
2025-09-09 21:54:15 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 102 ms, Memory: 59,776 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.py
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.py
@@ -1,12 +1,43 @@
-T=int(input())
-for tc in range(1,T+1):
-  N=int(input())
-  data=list(map(int,input().split()))
-  dp=[1]*N
-  for i in range(1,N):
-    mx=dp[i]
-    for j in range(i-1,-1,-1):
-      if data[j]<data[i]:
-        mx=max(mx,dp[j]+1)
-    dp[i]=mx
-  print(f'#{tc} {max(dp)}')
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    static int N;
+    static int[] arr, dp;
+    static int result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            N = Integer.parseInt(br.readLine());
+            arr = new int[N];
+            dp = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            for (int i = 0; i < N; i++) {
+                dp[i] = 1;
+                for (int j = 0; j < i; j++) {
+                    if (arr[j] < arr[i]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+
+            result = 0;
+            for (int i = 0; i < N; i++) {
+                result = Math.max(result, dp[i]);
+            }
+            System.out.println("#" + tc + " " + result);
+        }
+    }
+
+}
