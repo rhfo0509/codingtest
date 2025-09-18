74889a8c43d042fa5478faca101145466a7dc3c0
KIM EUNGSEO <ungnam0509@gmail.com>
2025-09-18 21:40:56 +0900

    [Gold IV] Title: 서강그라운드, Time: 116 ms, Memory: 14464 KB -BaekjoonHub

---
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n, m, r;
+    static int[] item;
+    static int[][] graph;
+    static int result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        item = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            item[i] = Integer.parseInt(st.nextToken());
+        }
+
+        graph = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(graph[i], 10_000);
+            graph[i][i] = 0;
+        }
+        for (int i = 1; i <= r; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int l = Integer.parseInt(st.nextToken());
+
+            graph[a][b] = l;
+            graph[b][a] = l;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (graph[i][k] + graph[k][j] < graph[i][j]) {
+                        graph[i][j] = graph[i][k] + graph[k][j];
+                    }
+                }
+            }
+        }
+
+        result = 0;
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = 1; j <= n; j++) {
+                if (graph[i][j] <= m) sum += item[j];
+            }
+            result = Math.max(result, sum);
+        }
+
+        System.out.print(result);
+    }
+}
