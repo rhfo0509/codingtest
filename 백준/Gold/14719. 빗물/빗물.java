2c0decf5066d0de61dc546d440c7546b9af77f90
KIM EUNGSEO <ungnam0509@gmail.com>
2025-10-15 11:00:12 +0900

    [Gold V] Title: 빗물, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -10,29 +10,28 @@ public class Main {
         int H = Integer.parseInt(st.nextToken());
         int W = Integer.parseInt(st.nextToken());
         int[] arr = new int[W];
+        int[] lMaxH = new int[W];
+        int[] rMaxH = new int[W];
 
         st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < W; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+
+            if (i == 0) lMaxH[i] = arr[i];
+            else lMaxH[i] = Math.max(lMaxH[i - 1], arr[i]);
+        }
+
+        for (int i = W - 1; i >= 0; i--) {
+            if (i == W - 1) rMaxH[i] = arr[i];
+            else rMaxH[i] = Math.max(rMaxH[i + 1], arr[i]);
         }
 
         // 총 쌓인 빗물의 양
         int result = 0;
         for (int i = 0; i < W; i++) {
-
-            int lMaxH = arr[i];
-            for (int j = i - 1; j >= 0; j--) {
-                lMaxH = Math.max(lMaxH, arr[j]);
-            }
-
-            int rMaxH = arr[i];
-            for (int j = i + 1; j < W; j++) {
-                rMaxH = Math.max(rMaxH, arr[j]);
-            }
-
-            int maxH = Math.min(lMaxH, rMaxH);
-            if (maxH > arr[i]) result += maxH - arr[i];
+            int h = arr[i];
+            int maxH = Math.min(lMaxH[i], rMaxH[i]);
+            if (h < maxH) result += maxH - h;
         }
 
         System.out.println(result);
