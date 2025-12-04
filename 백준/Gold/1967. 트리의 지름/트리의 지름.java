277d7e02a7e0e44308925872c09615a6a8625c69
ungnam <ungnam0509@gmail.com>
2025-12-04 18:05:38 +0900

    [Gold IV] Title: 트리의 지름, Time: 200 ms, Memory: 22568 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int n, diameter;
+    static int n, m, diameter;
     static List<List<Edge>> graph;
     static class Edge {
         int to;
@@ -11,7 +11,7 @@ public class Main {
             this.weight = weight;
         }
     }
-    static boolean[] visited, isLeafNode;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -20,8 +20,6 @@ public class Main {
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
-        isLeafNode = new boolean[n + 1];
-        Arrays.fill(isLeafNode, true);
         for (int i = 0; i < n - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
@@ -29,22 +27,26 @@ public class Main {
             int weight = Integer.parseInt(st.nextToken());
             graph.get(from).add(new Edge(to, weight));
             graph.get(to).add(new Edge(from, weight));
-            isLeafNode[from] = false;
         }
+
+        // 루트 노드 1로부터 가장 먼 정점
+        m = 1;
+        diameter = 0;
+        visited = new boolean[n + 1];
+        visited[1] = true;
+        dfs(1, 0);
 
         diameter = 0;
         visited = new boolean[n + 1];
-        for (int i = 1; i <= n; i++) {
-            // 리프 노드인 경우 지름의 끝 지점이 될 수 있다.
-            if (isLeafNode[i]) {
-                visited[i] = true;
-                dfs(i, 0);
-            }
-        }
+        visited[m] = true;
+        dfs(m, 0);
         System.out.print(diameter);
     }
     static void dfs(int cur, int length) {
-        diameter = Math.max(diameter, length);
+        if (diameter < length) {
+            diameter = length;
+            m = cur;
+        }
         for (Edge e : graph.get(cur)) {
             if (!visited[e.to]) {
                 visited[e.to] = true;
