5932f55be42eeffd16339d846ba814ddf0a3a21f
KIM EUNGSEO <ungnam0509@gmail.com>
2026-02-10 17:14:28 +0900

    [Silver II] Title: N과 M (11), Time: 316 ms, Memory: 33648 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr, ans;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        ans = new int[M];
+        backtracking(0);
+        System.out.print(sb);
+    }
+    static void backtracking(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int prev = -1;
+        for (int i = 0; i < N; i++) {
+            if (prev != arr[i]) {
+                prev = arr[i];
+                ans[depth] = arr[i];
+                backtracking(depth + 1);
+            }
+        }
+    }
+}
