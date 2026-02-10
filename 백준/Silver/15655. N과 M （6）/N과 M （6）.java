1250dd4199445cd8986ee49c1c1494361c436c0f
KIM EUNGSEO <ungnam0509@gmail.com>
2026-02-10 16:36:14 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,54 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static List<Integer> cur;
-	static int[] arr;
-	static boolean[] visited;
-	static StringBuilder sb;
-	
-	public static void dfs(int start, int length) {
-		if (length == M) {
-			for (Integer i : cur) {
-				sb.append(i).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for (int i = start; i < N; i++) {
-			cur.add(arr[i]);
-			dfs(i + 1, length + 1);
-			cur.remove(cur.size() - 1);
-		}
-	}
+    static int N, M;
+    static int[] arr, ans;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        ans = new int[M];
+        visited = new boolean[N];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		
-		cur = new ArrayList<>();
-		visited = new boolean[N];
-		sb = new StringBuilder();
-		
-		dfs(0, 0);
-		System.out.println(sb);
-	}
-
+        backtracking(0, 0);
+        System.out.print(sb);
+    }
+    static void backtracking(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                ans[depth] = arr[i];
+                backtracking(i + 1, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
 }
