cab3e59a67d3c7a99a61243c0010960a1b7277ec
KIM EUNGSEO <ungnam0509@gmail.com>
2026-02-10 16:48:38 +0900

    [Silver III] Title: N과 M (8), Time: 128 ms, Memory: 15780 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -5,46 +5,35 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static int[] data, cur;
-//	static boolean[] visited;
-	static StringBuilder sb;
-	
-	public static void dfs(int start, int depth) {
-		if (depth == M) {
-			for (int i : cur) {
-				sb.append(i).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for (int i = start; i < N; i++) {
-			cur[depth] = data[i];
-			dfs(i, depth + 1);
-		}
-	}
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		data = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			data[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(data);
-		
-		cur = new int[M];
-//		visited = new boolean[N];
-		sb = new StringBuilder();
-		
-		dfs(0, 0);
-		System.out.println(sb);
-	}
-
+    static int N, M;
+    static int[] arr, ans;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        ans = new int[M];
+        backtracking(0, 0);
+        System.out.print(sb);
+    }
+    static void backtracking(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i < N; i++) {
+            ans[depth] = arr[i];
+            backtracking(i, depth + 1);
+        }
+    }
 }
