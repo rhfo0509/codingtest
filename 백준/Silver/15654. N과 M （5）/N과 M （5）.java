f513150c85c7289363b103b91dc3cb243129ced1
KIM EUNGSEO <ungnam0509@gmail.com>
2026-02-10 16:28:00 +0900

    [Silver III] Title: N과 M (5), Time: 296 ms, Memory: 29060 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr, ans;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        ans = new int[M];
+        visited = new boolean[N];
+        backtracking(0);
+        System.out.print(sb);
+    }
+    static void backtracking(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                ans[depth] = arr[i];
+                backtracking(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
