d73e7201b37e7905762c525dca567e4d68bb3cb5
KIM EUNGSEO <ungnam0509@gmail.com>
2026-02-10 15:34:10 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        visited = new boolean[N + 1];
+
+        backtracking(1, 0);
+    }
+    static void backtracking(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = start; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i;
+                backtracking(i + 1, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
