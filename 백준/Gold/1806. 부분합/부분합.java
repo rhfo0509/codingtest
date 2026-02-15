52eb18a3ef08f2f052dcef3b6d0307de2f184b10
KIM EUNGSEO <ungnam0509@gmail.com>
2026-02-15 17:27:18 +0900

    [Gold IV] Title: 부분합, Time: 248 ms, Memory: 23820 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,44 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int N, S;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int S = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		int[] arr = new int[N];
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		// two-pointer
-		int start = 0;
-		int end = 0;
-		
-		// 현재 부분합
-		int sum = arr[start];
-		
-		// S 이상인 부분합 중 가장 짧은 것의 길이
-		int result = Integer.MAX_VALUE;
-		
-		for (start = 0; start < N; start++) {
-			while (end < N && sum < S) {
-				end++;
-				if (end == N) break;
-				sum += arr[end];
-			}
-			if (end == N) break;
-			result = Math.min(result, end-start+1);
-			sum -= arr[start];
-		}
-		
-		System.out.print(result == Integer.MAX_VALUE ? 0 : result);
-	}
+        int res = N + 1;
+        int cur = arr[0];
+        int start, end = 0;
+        for (start = 0; start < N; start++) {
+            while (end < N && cur < S) {
+                end++;
+                if (end != N) {
+                    cur += arr[end];
+                }
+            }
+            if (end == N) {
+                break;
+            }
+            res = Math.min(res, end - start + 1);
+            cur -= arr[start];
+        }
 
+        if (res == N + 1) {
+            System.out.print(0);
+        } else {
+            System.out.print(res);
+        }
+    }
 }
