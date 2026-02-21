4890b6ebd024c2d81b6eb5ed7302e242a56c3a9c
KIM EUNGSEO <ungnam0509@gmail.com>
2026-02-22 00:19:05 +0900

    [Bronze III] Title: 첨탑 밀어서 부수기, Time: 812 ms, Memory: 349640 KB -BaekjoonHub

---
+++ 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int cnt = 1;
+        int prev = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (prev <= arr[i]) cnt++;
+            prev = arr[i];
+        }
+        System.out.print(cnt);
+    }
+}
