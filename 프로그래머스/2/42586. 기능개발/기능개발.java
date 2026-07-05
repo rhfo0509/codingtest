ef27fd021568b867a097c77e57bd73e9ec5c22c0
KIM EUNGSEO <ungnam0509@gmail.com>
2026-07-05 20:32:50 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int n = speeds.length;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            int remain = 100 - progresses[i];
+            if (remain % speeds[i] == 0) {
+                arr[i] = remain / speeds[i];
+            } else {
+                arr[i] = (remain / speeds[i]) + 1;
+            }
+        }
+        Deque<Integer> stack = new ArrayDeque<>();
+        List<Integer> li = new ArrayList<>();
+        int cur = arr[0];
+        int cnt = 1;
+        for (int i = 1; i < n; i++) {
+            if (cur < arr[i]) {
+                li.add(cnt);
+                cur = arr[i];
+                cnt = 1;
+            } else {
+                cnt++;
+            }
+        }
+        li.add(cnt);
+        int[] res = new int[li.size()];
+        for (int i = 0; i < li.size(); i++) {
+            res[i] = li.get(i);
+        }
+        return res;
+    }
+}
