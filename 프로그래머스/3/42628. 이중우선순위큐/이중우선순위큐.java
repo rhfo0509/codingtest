6eb1fb944ecbeb191867035f292134380cdbe3e3
KIM EUNGSEO <ungnam0509@gmail.com>
2026-07-15 18:42:29 +0900

    [level 3] Title: 이중우선순위큐, Time: 36.32 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,27 +1,30 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
-        TreeSet<Integer> dpq = new TreeSet<>();
+        TreeMap<Integer, Integer> dpq = new TreeMap<>();
         for (int i = 0; i < operations.length; i++) {
             StringTokenizer st = new StringTokenizer(operations[i]);
             String cmd = st.nextToken();
             if (cmd.equals("I")) {
-                String n = st.nextToken();
-                dpq.add(Integer.parseInt(n));
+                int n = Integer.parseInt(st.nextToken());
+                dpq.put(n, dpq.getOrDefault(n, 0) + 1);
             } else {
                 if (dpq.isEmpty()) continue;
                 String n = st.nextToken();
+                int target = 0;
                 if (n.equals("1")) {
-                    dpq.pollLast();
+                    target = dpq.lastKey();
                 } else {
-                    dpq.pollFirst();
+                    target = dpq.firstKey();
                 }
+                dpq.put(target, dpq.get(target) - 1);
+                if (dpq.get(target) == 0) dpq.remove(target);
             }
         }
         int[] answer = new int[2];
         if (!dpq.isEmpty()) {
-            answer[0] = dpq.last();
-            answer[1] = dpq.first();
+            answer[0] = dpq.lastKey();
+            answer[1] = dpq.firstKey();
         }
         return answer;
     }
